6af04d1234aade52be2224de533fa8a9904d0601
Brandon Moberg <brandonmob3rg@gmail.com>
2013-07-30 23:01:58 -0500

    added Foundation

--- wp-content/themes/edu-clean/index-home.php
+++
@@ -1,8 +0,0 @@
-<?php /* Template Name: Homepage */
-get_header();
-?>
-
-<?php locate_template( array( 'lib/templates/wp-template/front-left.php'), true ); ?>
-<?php locate_template( array( 'lib/templates/wp-template/front-right.php'), true ); ?>
-
-<?php get_footer(); ?>
--- wp-content/themes/edu-clean/themes/edu-clean-child/_inc/css/readme.txt
+++
@@ -1,4 +0,0 @@
-rename child-style-sample to child-style to use child theme styling.
-
-
-36617-1375208317
